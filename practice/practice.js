151264a0051ef33e63c21b2647e0126e9a12d765
nasim-hoseini <nasimhoseini7337@gmail.com>
2023-10-24 15:29:39 +0330

    new file

--- practice/practice.js
+++ practice/practice.js
@@ -1,9 +1,9 @@
-// const data = parseInt(prompt("please enter number"));
+const data = parseInt(prompt("please enter number"));
 
-// if (typeof data === "number") {
-//   alert("ok");
-// } else if (typeof data === "string") {
-//   alert("else if ok");
-// } else {
-//   alert("no");
-// }
+if (typeof data === "number") {
+  alert("ok");
+} else if (typeof data === "string") {
+  alert("else if ok");
+} else {
+  alert("no");
+}
