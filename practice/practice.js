5274610b45082a1af21479ccf1f3aad2b5e8efa1
nasim-hoseini <nasimhoseini7337@gmail.com>
2023-10-24 15:28:30 +0330

    nasim file branch develop

---
+++ practice/practice.js
@@ -0,0 +1,9 @@
+// const data = parseInt(prompt("please enter number"));
+
+// if (typeof data === "number") {
+//   alert("ok");
+// } else if (typeof data === "string") {
+//   alert("else if ok");
+// } else {
+//   alert("no");
+// }
